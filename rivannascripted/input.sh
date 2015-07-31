9657c8a8ceef7113809a208ce5fd326fcaf19591
zackcarson <zack.carson@virginia.edu>
2015-07-31 14:08:58 -0400

    Update input.sh

--- rivannascripted/input.sh
+++ rivannascripted/input.sh
@@ -6,3 +6,12 @@ economy
 6000
 387540
 zrc2hs
+
+#First, enter the queue you want to submit to
+#Then, the number of events to run per job
+#Then, the number of jobs you want to submit to Rivanna
+#Then, the number of nodes to run on per job
+#Then, the number of tasks to run on each core per job
+#Then, the number of Megabytes of RAM to allocate to each core
+#Then, a random integer 6 digits long to use for the seed
+#Lastly, your Rivanna username
